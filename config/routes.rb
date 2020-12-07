7f73d904fa04868d6f32587a168e366f94d3e037
shin-ueda000 <73635333+shin-ueda000@users.noreply.github.com>
2020-12-07 12:59:16 +0900

    トップページの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'infos/index'
+  root to: 'infos#index'
+  resources :infos, only: :index
 end
