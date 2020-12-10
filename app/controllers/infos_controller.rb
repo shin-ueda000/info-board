cdcddb3da8e09cdec03a0a9df95eea7c2c35a52a
shin-ueda000 <73635333+shin-ueda000@users.noreply.github.com>
2020-12-10 10:20:23 +0900

    fromの実装

--- app/controllers/infos_controller.rb
+++ app/controllers/infos_controller.rb
@@ -2,8 +2,27 @@ class InfosController < ApplicationController
   before_action :authenticate_user!,　except: [:index]
 
   def index
+    @info = Info.all
+  end
+
+  def new
     @info = Info.new
   end
 
+  def create
+    @info = Info.new(info_params)    
+    if @info.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+
+  private
+  def info_params
+    params.require(:info).permit(:title, :content, :image ).merge(user_id: current_user.id)
+  end
   
 end
